c9a17be3f1bcba443b660a38d2d94cd5dc0f13c7
Ahmad A <ahmad@dataprism.me>
2022-11-25 21:39:54 +0500

    added fluentui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { FluentProvider, teamsLightTheme } from "@fluentui/react-components";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <FluentProvider theme={teamsLightTheme}>
+      <App />
+    </FluentProvider>
+    
   </React.StrictMode>
-)
+);
