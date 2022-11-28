35c84dfe95459e6f8507aa4be559775efd09faea
Ahmad A <ahmad@dataprism.me>
2022-11-28 10:27:54 +0500

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import { Button } from "@fluentui/react-components";
 import "./App.css";
+import Login from "./Pages/Login";
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <Button appearance="primary">Get started</Button>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
