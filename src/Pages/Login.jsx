c334a176b67a2b20f952b480dbdbbc39547af81b
Ahmad A <ahmad@dataprism.me>
2022-11-28 12:16:01 +0500

    fixed sizes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,23 +2,25 @@ import React, { useState } from "react";
 import { Button, Input, Label, Text } from "@fluentui/react-components";
 import { AiTwotoneLock } from "react-icons/ai";
 import "./Login.css";
-import Fade from "react-reveal/Pulse";
+import Fade from "react-reveal/Fade";
 export default function Login() {
   const [secureCode, setSecureCode] = useState(0);
   return (
     <div id="main">
-      <Fade >
+      <Fade top>
         <div id="container">
           <Label id="label">Secure Code</Label>
           <Input
             id="input"
+            size="large"
+            type="password"
             contentBefore={<AiTwotoneLock />}
             title="Enter Secure Code"
             onChange={(x) => {
               setSecureCode(x.target.value);
             }}
           />
-          <Button id="button" onClick={() => {}}>
+          <Button id="button" size="large" onClick={() => {}}>
             Login
           </Button>
         </div>
